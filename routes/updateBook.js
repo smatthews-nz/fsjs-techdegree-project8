5c5c75c3030b633b9c4d75c951b527220ae40d0d
Sam Matthews <sam@subsupps.co.nz>
2019-08-29 12:05:30 +1200

    Fixes error messaging for updating book details

--- routes/updateBook.js
+++ routes/updateBook.js
@@ -36,7 +36,23 @@ router.post('/book/:id', async(req, res) => {
                 res.redirect('/books')
             })
         } catch (error){
-            console.error('Error updating book', error)
+            if(error.name === 'SequelizeValidationError'){
+                const errors = error.errors
+                const book = {
+                    id: req.body.id,
+                    title: req.body.title,
+                    author: req.body.author,
+                    genre: req.body.genre,
+                    year: req.body.year
+                }
+                res.render('update-book', { book , errors})
+            } else {
+                //if not sequelize error then res error template
+                console.error(error)
+                error.status = 500;
+                error.message = "Book could not bed added to the library database"
+                next(error);
+            }
         }
 
 })
